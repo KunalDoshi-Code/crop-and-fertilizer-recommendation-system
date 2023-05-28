6bb9fdb5b4525fef8960b99dd27a9661290d3d2a
Kunal Doshi <60850461+KunalDoshi-Code@users.noreply.github.com>
2023-05-28 11:18:58 +0530

    Add files via upload

--- crop_recommendation_app/urls.py
+++ crop_recommendation_app/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -10,3 +12,6 @@ urlpatterns = [
     path("crop_result/", views.crop_result, name="crop_result"),
     path("fertilizer_result/", views.fertilizer_result, name="fertilizer_result"),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
