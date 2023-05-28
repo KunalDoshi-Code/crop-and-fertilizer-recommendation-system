716177a0ec4b85e3bc65a6a3dc692a064cc4326a
Kunal Doshi <60850461+KunalDoshi-Code@users.noreply.github.com>
2023-05-28 11:44:46 +0530

    Add files via upload

--- crop_recommendation_app/settings.py
+++ crop_recommendation_app/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-a7f+ows$6rx=me!)0_bp@6_d1#w6(-_6&(p#@6z7d34z#w%b*^"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
 
